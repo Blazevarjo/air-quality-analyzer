5c25a6fc82f3ea8d2331bf06432c3a955cc05c8a
Jan Rubel <jasiekrubel@gmail.com>
2021-01-11 11:36:12 +0100

    Fix y axis formatting

--- app/src/main/java/com/example/airqualityanalyzer/utils/YAxisUnitFormatter.kt
+++ app/src/main/java/com/example/airqualityanalyzer/utils/YAxisUnitFormatter.kt
@@ -6,6 +6,12 @@ import com.github.mikephil.charting.formatter.ValueFormatter
 class YAxisUnitFormatter : ValueFormatter() {
 
     override fun getAxisLabel(value: Float, axis: AxisBase?): String {
-        return "$value μg/m3"
+        return "${value.round(2)} μg/m3"
+    }
+
+    private fun Float.round(decimals: Int): Float {
+        var multiplier = 1.0
+        repeat(decimals) { multiplier *= 10 }
+        return (kotlin.math.round(this * multiplier) / multiplier).toFloat()
     }
 }
